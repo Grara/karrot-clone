45d59efd79757c16dee6e8368bfd3173847af8dc
Grara <nomj18@gmail.com>
2023-02-21 21:52:54 +0900

    Start

---
+++ src/main/java/com/karrotclone/KarrotcloneApplication.java
@@ -0,0 +1,13 @@
+package com.karrotclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrotcloneApplication.class, args);
+	}
+
+}
