a3260b6b5ee616e4e35b28420150e37249006e71
gyoheak <gyoheak_oh@tmax.co.kr>
2023-03-06 17:05:18 +0900

    fix(details): CustomUserDetailsService

---
+++ src/main/java/com/karrotclone/domain/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.karrotclone.domain;
+
+import com.karrotclone.repository.TempMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final TempMemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> _user = memberRepository.findByEmail(username);
+        if(_user.isEmpty()){
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        Member user = _user.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+        return new Member(user.getId(), user.getEmail(), user.getPassword(), authorities, user.getNickName());
+    }
+}
