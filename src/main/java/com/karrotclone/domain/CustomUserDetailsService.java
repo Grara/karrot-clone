8036bd6a18f0c89e5a740cc3bd63efa6d0c2a8fa
gyoheak <gyoheak@gyoheakui-MacBookAir.local>
2023-03-06 09:55:19 +0900

    fix(temp): temp fix to window

--- src/main/java/com/karrotclone/domain/CustomUserDetailsService.java
+++ src/main/java/com/karrotclone/domain/CustomUserDetailsService.java
@@ -1,18 +1,37 @@
 package com.karrotclone.domain;
 
+import com.karrotclone.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final MemberRepositoy memberRepositoy;
+    private final MemberRepository memberRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberR
+        Optional<Member> member = memberRepository.findByEmail(username);
+        if(member.isEmpty()){
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        Member user = member.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getAuthorities().toString()));
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getAuthorities().toArray(new String[0]))
+                .build();
     }
 }
