5e348d9007b410f14c7d8a5a115f892b467bcf47
gyoheak <gyoheak_oh@tmax.co.kr>
2023-03-06 16:33:10 +0900

    fix(util): util and entry point

---
+++ src/main/java/com/karrotclone/utils/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.karrotclone.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+
+    private SecurityUtil(){}
+
+    // Security Context에 유저 정보가 저장되는 지점
+    // Request가 들어올 때 jwtFilter의 doFilter에서 저장
+    public static Long getCurrentUserId(){
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || authentication.getName() == null){
+            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+        }
+
+        return Long.parseLong(authentication.getName());
+    }
+}
