af581c96a644cf3a197347eab5fe7a2d131a91f8
Grara <nomj18@gmail.com>
2023-03-22 18:56:36 +0900

    :art: [FIX] 회원가입, 로그인, 로그아웃 시 반환값 추가

--- src/main/java/com/karrotclone/api/LoginController.java
+++ src/main/java/com/karrotclone/api/LoginController.java
@@ -3,11 +3,14 @@ package com.karrotclone.api;
 import com.karrotclone.config.jwt.JwtTokenProvider;
 import com.karrotclone.domain.Member;
 import com.karrotclone.dto.LoginDto;
+import com.karrotclone.dto.ResponseDto;
 import com.karrotclone.service.auth.LoginService;
 import com.karrotclone.utils.CookieUtil;
 import com.karrotclone.utils.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,39 +36,42 @@ public class LoginController {
     private final RedisUtil redisUtil;
 
     @PostMapping("api/v1/login")
-    public void login(@RequestBody LoginDto user, HttpServletRequest req, HttpServletResponse res){
-        try{
-            final Member member = loginService.login(user.getEmail(), user.getPassword());
-            Cookie accessToken = cookieUtil.creatCookie(jwtTokenProvider.ACCESS_TOKEN_NAME,jwtTokenProvider.generateAccessToken(member));
-            accessToken.setMaxAge((int) TimeUnit.MILLISECONDS.toSeconds(jwtTokenProvider.ACCESS_TOKEN_EXPIRE_TIME));
-            Cookie refreshToken = cookieUtil.creatCookie(jwtTokenProvider.REFRESH_TOKEN_NAME, jwtTokenProvider.generateRefreshToken(member));
-            refreshToken.setMaxAge((int) TimeUnit.MILLISECONDS.toSeconds(jwtTokenProvider.REFRESH_TOKEN_EXPIRE_TIME));
-            redisUtil.setDataExpire(refreshToken.getValue(), member.getUsername(), jwtTokenProvider.REFRESH_TOKEN_EXPIRE_TIME);
-            res.addCookie(accessToken);
-            res.addCookie(refreshToken);
+    public ResponseEntity<ResponseDto> login(@RequestBody LoginDto user, HttpServletRequest req, HttpServletResponse res) {
 
-        } catch(Exception e){
-            log.error("login error", e);
-        }
+        final Member member = loginService.login(user.getEmail(), user.getPassword());
+        Cookie accessToken = cookieUtil.creatCookie(jwtTokenProvider.ACCESS_TOKEN_NAME, jwtTokenProvider.generateAccessToken(member));
+        accessToken.setMaxAge((int) TimeUnit.MILLISECONDS.toSeconds(jwtTokenProvider.ACCESS_TOKEN_EXPIRE_TIME));
+        Cookie refreshToken = cookieUtil.creatCookie(jwtTokenProvider.REFRESH_TOKEN_NAME, jwtTokenProvider.generateRefreshToken(member));
+        refreshToken.setMaxAge((int) TimeUnit.MILLISECONDS.toSeconds(jwtTokenProvider.REFRESH_TOKEN_EXPIRE_TIME));
+        redisUtil.setDataExpire(refreshToken.getValue(), member.getUsername(), jwtTokenProvider.REFRESH_TOKEN_EXPIRE_TIME);
+        res.addCookie(accessToken);
+        res.addCookie(refreshToken);
+
+        ResponseDto resDto = new ResponseDto();
+        resDto.setMessage("로그인에 성공했습니다. JWT토큰은 쿠키에 저장되었습니다.");
+        return new ResponseEntity<>(resDto, HttpStatus.OK);
     }
 
     @GetMapping("api/v1/logout")
-    public String logout(HttpServletRequest req, HttpServletResponse res){
+    public ResponseEntity<ResponseDto> logout(HttpServletRequest req, HttpServletResponse res) {
         SecurityContextHolder.clearContext();
         Cookie accessToken = cookieUtil.getCookie(req, jwtTokenProvider.ACCESS_TOKEN_NAME);
         Cookie refreshToken = cookieUtil.getCookie(req, jwtTokenProvider.REFRESH_TOKEN_NAME);
-        if(accessToken != null){
+        if (accessToken != null) {
             Long expiration = jwtTokenProvider.getExpireTime(accessToken.getValue());
-            redisUtil.setBlackList(accessToken.getValue(), "accessToken", expiration-System.currentTimeMillis());
+            redisUtil.setBlackList(accessToken.getValue(), "accessToken", expiration - System.currentTimeMillis());
             accessToken.setMaxAge(0);
             res.addCookie(accessToken);
         }
-        if(refreshToken != null){
+        if (refreshToken != null) {
             refreshToken.setMaxAge(0);
             res.addCookie(refreshToken);
             redisUtil.deleteData(refreshToken.getValue());
         }
 
-        return null;
+        ResponseDto resDto = new ResponseDto();
+        resDto.setMessage("로그아웃에 성공했습니다.");
+
+        return new ResponseEntity<>(resDto, HttpStatus.OK);
     }
 }
