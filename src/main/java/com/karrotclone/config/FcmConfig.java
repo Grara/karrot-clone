a9f56fce6c3d00fc96695705ce0df5d224ad0138
Grara <nomj18@gmail.com>
2023-04-11 21:53:03 +0900

    FCM 수정

--- src/main/java/com/karrotclone/config/FcmConfig.java
+++ src/main/java/com/karrotclone/config/FcmConfig.java
@@ -1,6 +1,7 @@
 package com.karrotclone.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
@@ -27,6 +28,9 @@ public class FcmConfig {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(stream))
                     .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
         }
     }
 }
