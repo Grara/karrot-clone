90b1a5f90c7fcc2212b06e83e71766ab873227f9
Grara <nomj18@gmail.com>
2023-05-05 18:45:21 +0900

    스크립트 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/karrot/temp/build/libs
-TEMP_REPOSITORY=/home/ec2-user/app/karrot/temp
+TEMP_REPOSITORY=/home/ec2-user/app/karrot
 
 echo "> 현재 구동중인 PID확인"
 
